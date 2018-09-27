b27ec7480e63e7e185cb7cf503344837666e8c8a
Nishant <nishu.hooda929@gmail.com>
2018-09-27 13:09:16 +0530

    Changed template folder location for pythonanywhere and added dummy login,signup and inbox pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 #URLs start from here
 urlpatterns = [
     path('',views.post_list,name='post list'),
+    path('register',views.register,name='register'),
+    path('inbox',views.inbox,name='inbox'),
+    path('login',views.login,name='login'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,14 @@ from django.utils import timezone
 from .models import Post
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(publish_date__lte=timezone.now()).order_by('publish_date')
+    posts = Post.objects.order_by('publish_date')
     return render(request,'blog/post_list.html',{'posts':posts})
+
+def register(request):
+    return render(request,'blog/signup.html',{})
+
+def inbox(request):
+    return render(request,'blog/inbox.html',{})
+
+def login(request):
+    return render(request,'blog/login.html',{})
