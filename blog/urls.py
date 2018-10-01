0839c141bb6d949118e1523e608aa349f7eb51e8
Nishant <nishu.hooda929@gmail.com>
2018-10-01 23:31:51 +0530

    Added view, template for detailed post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
+
 #URLs start from here
 urlpatterns = [
     path('',views.post_list,name='post list'),
     path('register',views.register,name='register'),
     path('inbox',views.inbox,name='inbox'),
     path('login',views.login,name='login'),
-
+    path('post/<int:pk>)/', views.post_detail, name='post_detail'),
 ]
